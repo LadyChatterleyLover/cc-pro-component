0f7c1c080e90acbc7c599c4fd7dab49252f015e5
五月的夏天 <285258675@qq.com>
2022-05-08 23:08:40 +0800

    feat: 初始化项目并配置git提交配置

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang='ts' setup>
+</script>
+
+<style lang='scss'>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>home</div>
+</template>
+
+<script lang='ts' setup>
+</script>
+
+<style lang='scss' scoped>
+
+</style>
