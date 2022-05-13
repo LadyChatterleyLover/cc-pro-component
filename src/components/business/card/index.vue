9599a3f74d87f020b845711180c474a0fe756768
骆沛 <luopei@11.com>
2022-05-13 16:23:48 +0800

    feat: 新增业务卡片组件

---
+++ src/components/business/card/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-card>
+    <template #header>
+      <el-row justify="space-between">
+        <el-col :span="12">
+          <slot name="title" v-if="$slots.title"></slot>
+          <div v-else>{{ title }}</div>
+        </el-col>
+        <el-col :span="12" style="text-align: right">
+          <el-tag :type="tagType">{{ tag }}</el-tag>
+        </el-col>
+      </el-row>
+    </template>
+    <el-row justify="space-between" style="padding: 16px">
+      <el-col :span="12" style="font-size: 18px">
+        <slot name="value" v-if="$slots.value"></slot>
+        <div v-else>{{ value }}</div>
+      </el-col>
+      <el-col :span="12" style="text-align: right">
+        <slot name="image" v-if="$slots.image"></slot>
+        <div v-else>{{ image }}</div>
+      </el-col>
+    </el-row>
+    <el-row justify="space-between" style="padding: 8px 16px">
+      <el-col :span="12">
+        <slot name="descTitle" v-if="$slots.descTitle"></slot>
+        <div v-else>{{ descTitle }}</div>
+      </el-col>
+      <el-col :span="12" style="text-align: right">
+        <slot name="desc" v-if="$slots.desc"></slot>
+        <div v-else>{{ desc }}</div>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title: string
+    tag?: string
+    tagType: '' | 'success' | 'info' | 'warnin' | 'danger'
+    value?: number | string
+    image?: string
+    descTitle?: string
+    desc?: string
+  }>(),
+  {
+    tagType: '',
+    value: '',
+    image: '',
+    descTitle: '',
+    desc: '',
+  }
+)
+</script>
+
+<style scoped></style>
--- src/views/dashboard/analysis/index.vue
+++ src/views/dashboard/analysis/index.vue
@@ -1,10 +1,64 @@
 <template>
-  <div>analysis</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6" v-for="(item, index) in list" :key="index">
+        <card
+          :title="item.title"
+          :tag="item.tag"
+          :value="item.value"
+          :tagType="item.tagType"
+          :descTitle="item.descTitle"
+          :desc="item.desc"
+        >
+          <template #image>
+            <img
+              style="width: 36px; height: 36px; border-radius: 50%"
+              src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+            />
+          </template>
+        </card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
+import card from '@/components/business/card/index.vue'
+
+const list: any = [
+  {
+    title: '访问数',
+    tag: '月',
+    tagType: '',
+    value: '$2,000',
+    descTitle: '总访问数',
+    desc: '$120,000',
+  },
+  {
+    title: '访问数',
+    tag: '月',
+    tagType: '',
+    value: '$2,000',
+    descTitle: '总访问数',
+    desc: '$120,000',
+  },
+  {
+    title: '访问数',
+    tag: '月',
+    tagType: '',
+    value: '$2,000',
+    descTitle: '总访问数',
+    desc: '$120,000',
+  },
+  {
+    title: '访问数',
+    tag: '月',
+    tagType: '',
+    value: '$2,000',
+    descTitle: '总访问数',
+    desc: '$120,000',
+  },
+]
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
